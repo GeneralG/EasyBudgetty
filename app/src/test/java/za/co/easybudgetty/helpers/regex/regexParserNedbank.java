8569dc6a008e612e1979b9d55606693df500f2ba
Tyler Dean Smith <tyler.smith.69@gmail.com>
2017-05-09 21:36:20 +0200

    Comments are test messages to be written in, to test logic

--- app/src/test/java/za/co/easybudgetty/helpers/regex/regexParserNedbank.java
+++ app/src/test/java/za/co/easybudgetty/helpers/regex/regexParserNedbank.java
@@ -6,6 +6,10 @@ import org.junit.Test;
  * Created by tyler on 2016/06/26.
  */
 public class regexParserNedbank {
+
+    //Nedbank: Electronic Banking EFT deposit of R3000.00 into acc **6134 Avail bal R10000.00 at 20:01 on 15May17. Reg: Monkeys need food
+    //Nedbank: ATM Cash withdrawal of R150.00 from acc **6134 @ ABSA FRENDLY DURBANVIDU. Avail bal R10000.00 at 20:01 on 15May17. Reg: Monkeys need food
+    //Nedbank: R294.20 purchased @ Checkers Durbanville S from acc **6134. Avail bal R123123.33 at 08:55 on 10May16
     @Test
     public void checkNumberValidility(String number)
     {
